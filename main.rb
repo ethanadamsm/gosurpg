845375b3a7dc4bce8bf5f29f032efccdd986fdb4
Ethan Adams <ethanadamsm@gmail.com>
2015-10-16 15:53:37 -0600

    Logic start, refactor start, keypress start

--- main.rb
+++ main.rb
@@ -1,19 +1,38 @@
 require "gosu"
-require_relative "map.rb"
+require_relative "map"
+require_relative "constants"
+require_relative "player"
 
 class GameWindow < Gosu::Window
 
 	def initialize
-		super 640, 480
+		super Constants::WWIDTH, Constants::WHEIGHT
 		@map = Map.new
+		@player = Player.new(Constants::WWIDTH / 2, Constants::WHEIGHT / 2)
 	end
 
 	def update
+		if button_down?(Gosu::KbUp)
+			@player.move_up
+		elsif button_down?(Gosu::KbDown)
+			@player.move_down
+		elsif button_down?(Gosu::KbLeft)
+			@player.move_left
+		elsif button_down?(Gosu::KbRight)
+			@player.move_right
+		end
 		
+		case @player.check_spot?
+			when 1  
+				@player.teleport_right
+			when 2
+				@player.teleport_left
+		end
 	end
 
 	def draw
 		@map.draw
+		@player.draw
 	end
 
 	def button_down(id)
