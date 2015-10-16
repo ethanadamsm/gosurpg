fbf771b4fa02294a20aac7793489c45aa6af2541
Ethan Adams <ethanadamsm@gmail.com>
2015-10-16 15:51:27 -0600

    Boundaries, movement, and player start

---
+++ player.rb
@@ -0,0 +1,67 @@
+require "gosu"
+require_relative "z_order"
+require_relative "constants"
+require_relative "map"
+
+class Player
+
+	def initialize(x, y) 
+		@animation = Gosu::Image.new("media/player.png")
+		@x = x
+		@y = y
+	end
+
+	def check_spot?
+		if @x + Constants::PWIDTH > Constants::WWIDTH - Constants::TWIDTH * 4
+			return 1
+		elsif @x < Constants::TWIDTH * 4
+			return 2
+		elsif @y + Constants::PHEIGHT > Constants::WHEIGHT - Constants::THEIGHT * 4 
+			return 3
+		elsif @y < Constants::THEIGHT * 4
+			return 4
+		end		
+	end
+
+	def teleport(x, y) 
+		@x = x
+		@y = y
+	end
+
+	def teleport_right
+		teleport((Constants::WWIDTH - Constants::TWIDTH * 4) - (Constants::PWIDTH - 1), @y)
+	end
+
+	def teleport_left
+		teleport((Constants::TWIDTH * 4) + 1, @y)
+	end
+
+	def teleport_up
+
+	end
+
+	def teleport_down
+
+	end
+
+	def move_left
+		@x -= Constants::MOVEMENT
+	end
+
+	def move_right
+		@x += Constants::MOVEMENT
+	end
+
+	def move_up
+		@y -= Constants::MOVEMENT
+	end
+
+	def move_down
+		@y += Constants::MOVEMENT
+	end
+
+	def draw
+		@animation.draw(@x, @y, ZOrder::PLAYER)
+	end
+
+end
