f6c8b91c6e3293e9b83c4470de8c0581dc415bc5
Ethan Adams <ethanadamsm@gmail.com>
2015-10-16 16:32:12 -0600

    Create getters and setters

--- tile.rb
+++ tile.rb
@@ -15,4 +15,20 @@ class Tile
 		@animation.draw(@x, @y, ZOrder::TILES)
 	end
 
+	def get_x
+		@x
+	end
+
+	def get_y
+		@y
+	end
+
+	def set_x(x)
+		@x = x
+	end
+
+	def set_y(y)
+		@y = y
+	end
+
 end
