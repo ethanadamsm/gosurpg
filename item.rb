bb1c3af8ef5d43ad8f12792162da27e38e397da6
Ethan Adams <ethanadamsm@s.dcsdk12.org>
2015-10-20 09:45:10 -0600

    Add item logic, draw, and move

--- item.rb
+++ item.rb
@@ -1,15 +1,42 @@
+require "gosu"
+require_relative "z_order"
+require_relative "constants"
+
 class Item
 
-	def initialize(name, number)
+	def initialize(name, number, x, y, w, h, ground, animation)
 		@name = name
 		@number = number 
-		@x = nil
-		@y = nil
-		@ground = false
+		@x = x
+		@y = y
+		@ground = ground
+		@animation = animation
 	end
 
 	def add_number(num)
 		@number += num
+	end
+
+	def move_left
+		@x += Constants::TWIDTH
+	end
+
+	def move_right 
+		@x -= Constants::TWIDTH
+	end
+
+	def get_x
+		@x
+	end
+
+	def get_y
+		@y
+	end
+
+	def draw
+		if @ground
+			@animation.draw(@x, @y, ZOrder::ITEMS)
+		end 
 	end
 
 end
