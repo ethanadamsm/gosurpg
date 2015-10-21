ba726e327cc1e3899f7cebfcc7f6fd483c7ac1d4
Ethan Adams <ethanadamsm@gmail.com>
2015-10-21 16:34:47 -0600

    Change item to include subclasses

--- item.rb
+++ item.rb
@@ -1,38 +1,15 @@
-require "gosu"
-require_relative "z_order"
 require_relative "constants"
-require_relative "hitbox"
 
 class Item
 
-	def initialize(name, number, x, y, w, h, ground, animation)
+	def initialize(name, number, animation)
 		@name = name
 		@number = number 
-		@x = x
-		@y = y
-		@ground = ground
 		@animation = animation
-		@hitbox = Hitbox.new(@x, @y, Constants::IWIDTH, Constants::IHEIGHT)
 	end
 
 	def add_number(num)
 		@number += num
-	end
-
-	def move_left
-		@x += Constants::TWIDTH
-	end
-
-	def move_right 
-		@x -= Constants::TWIDTH
-	end
-
-	def get_x
-		@x
-	end
-
-	def get_y
-		@y
 	end
 
 	def get_name 
@@ -43,22 +20,8 @@ class Item
 		@number
 	end
 
-	def set_ground(ground)
-		@ground = ground
-	end
-
-	def get_ground
-		@ground
-	end
-
-	def hitbox
-		@hitbox.hitbox
-	end
-
-	def draw
-		if @ground
-			@animation.draw(@x, @y, ZOrder::ITEMS)
-		end 
+	def get_animation
+		@animation
 	end
 
 end
