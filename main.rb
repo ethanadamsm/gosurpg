756489211a13c5ab9de7e3afaf93837a34c32075
Ethan Adams <ethanadamsm@s.dcsdk12.org>
2015-10-21 10:23:49 -0600

    Link code to collsion start

--- main.rb
+++ main.rb
@@ -8,20 +8,8 @@ class GameWindow < Gosu::Window
 
 	def initialize
 		super Constants::WWIDTH, Constants::WHEIGHT
-		filenames = open("media/names.txt")
-		filenames = filenames.read
-		@itemnames = filenames.split(",")
-		fileani = open("media/ani.txt")
-		fileani = fileani.read
-		fileani = fileani.split(", ")
-		@itemani = []
-		fileani.each do |element|
-			@itemani.push(Gosu::Image.new("media/" + element))
-		end
 		@map = Map.new
 		@player = Player.new(Constants::WWIDTH / 2, Constants::WHEIGHT / 2)
-		@items = []
-		create_items_random
 	end
 
 	def update
@@ -34,11 +22,8 @@ class GameWindow < Gosu::Window
 		elsif button_down?(Gosu::KbRight)
 			@player.move_right
 		elsif button_down?(Gosu::KbP)
-			@items.each do |element| 
-				if collision(@player.hitbox, element.hitbox)
-					@items[@items.index(element)].set_ground(false)
-					@player.add_item(element.get_name, element.get_number)
-				end
+			@map.get_items.each do |element| 
+				
 			end	
 		end
 
@@ -65,24 +50,10 @@ class GameWindow < Gosu::Window
 	def draw
 		@map.draw
 		@player.draw
-		@items.each do |element|
-			element.draw
-		end
 	end
 
 	def button_down(id)
 		close if id == Gosu::KbEscape
-	end
-
-	def create_items_random
-		(0...rand(5)).each do |n|
-			spot = rand(0...@itemnames.length)
-			@items.push(Item.new(@itemnames[spot], rand(3), rand(0...(Constants::WWIDTH - Constants::IWIDTH)), rand(0...(Constants::WHEIGHT - Constants::IHEIGHT)), Constants::IWIDTH, Constants::IHEIGHT, true, @itemani[spot]))
-		end
-	end
-
-	def collision(h1, h2)
-			return h1[0] < h2[0] + h2[2] && h1[0] + h1[2] > h2[0] && h1[1] + h1[3] > h2[1] && h1[1] < h2[1] + h2[3]
 	end
 
 end
