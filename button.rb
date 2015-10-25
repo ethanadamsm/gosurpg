deb9c5850ce94d65ce0f63766bb676f38c28ff4a
Ethan Adams <ethanadamsm@gmail.com>
2015-10-25 15:27:12 -0600

    Add button for gui use

---
+++ button.rb
@@ -0,0 +1,17 @@
+require "gosu"
+
+class Button
+
+	def initialize(x, y, w, h, text) 
+		@x = x
+		@y = y
+		@w = w
+		@h = height
+		@text = text
+	end
+
+	def draw
+		@button.draw(@x, @y, )
+	end
+
+end
