9bcca6d9bce10e01e8f0a3117ad458db5fe65424
Ethan Adams <ethanadamsm@gmail.com>
2015-10-24 19:07:36 -0600

    Fix draw, and add_item in inventory

--- inventory.rb
+++ inventory.rb
@@ -11,17 +11,25 @@ class Inventory
 	end	
 
 	def add_item(name, quantity, animation) 
-		@items.push(Item.new(name, quantity, animation))
+		item = @items.select { |item| item.get_name == name }.first
+		if item != nil
+			@items[@items.index(item)].add_number(quantity)
+		else
+			@items.push(Item.new(name, quantity, animation))
+		end
+		if @items.length == 0
+			@items.push(Item.new(name, quantity, animation))
+		end
 	end
 
 	def draw
 		@border.draw(20, Constants::WHEIGHT - 50, ZOrder::INVENTORY)
 		x = 25
 		text_x = 30
-		font = Gosu::Font.new(6)
+		font = Gosu::Font.new(12)
 		@items.each do |element|
 			element.get_animation.draw(x, Constants::WHEIGHT - 45, ZOrder::INVENTORY, 5/3, 5/3)
-			font.draw("#{element.get_number}", text_x, Constants::WHEIGHT - 60, 1, 1, 0xff_ffff00)
+			font.draw("#{element.get_number}", text_x, Constants::WHEIGHT - 60, ZOrder::TEXT, 1, 1, 0xff_ffff00)
 			x += 25
 			text_x += 25
 		end
