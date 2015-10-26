84769b68e8363f6597f4213dde6d0efa6a704704
Ethan Adams <ethanadamsm@s.dcsdk12.org>
2015-10-26 10:23:29 -0600

    Update logic

--- gui.rb
+++ gui.rb
@@ -3,20 +3,37 @@ require_relative "Box"
 
 class Gui
 
-	def initialize(width, height, x, y, text, moveable)
+	def initialize(width, height, x, y, text, moveable, visible)
 		@mainbox = Box.new(x, y, width, height, text, true, moveable)
 		@boxes = []
+		@mousex = 0
+		@mousey = 0
+		@visible = visible
 	end
 
 	def draw
-		@boxes.each do |box|
-			box.draw
+		if @visible
+			@boxes.each do |box|
+				box.draw
+			end
+			@mainbox.draw
 		end
-		@mainbox.draw
 	end
 
-	def add_button()
-		@mainbox.add_button
+	def set_visibility(visible)
+		@visible = visible
+	end
+
+	def add_button(x, y, w, h, text)
+		@mainbox.add_button(x, y, w, h, text)
+	end
+
+	def update(mousex, mousey)
+		if @visible
+			@mousex = mousex
+			@mousey = mousey 
+			return @mainbox.update(mousex, mousey)
+		end
 	end
 
 end
