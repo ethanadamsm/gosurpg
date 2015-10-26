ec8426f623890925c0da18845c5e2bd6fcb76262
Ethan Adams <ethanadamsm@s.dcsdk12.org>
2015-10-26 10:23:18 -0600

    Add collision? method

--- button.rb
+++ button.rb
@@ -1,4 +1,5 @@
 require "gosu"
+require_relative "z_order"
 
 class Button
 
@@ -6,12 +7,21 @@ class Button
 		@x = x
 		@y = y
 		@w = w
-		@h = height
+		@h = h
 		@text = text
+		@button = Gosu::Image.new("media/window.png")
+		@font = Gosu::Font.new(12)
 	end
 
 	def draw
-		@button.draw(@x, @y, )
+		scalex = @w / 100.0
+		scaley = @h / 100.0
+		@button.draw(@x, @y, ZOrder::BUTTON, scalex, scaley)
+		@font.draw(@text, @x + (@w / 2) - (@text.length * 6), @y + (@h / 2), ZOrder::BUTTON, )
+	end
+
+	def collision?(mousex, mousey)
+		mousex > @x && mousex < @x + @w && mousey > @y && mousey < @y + @h
 	end
 
 end
